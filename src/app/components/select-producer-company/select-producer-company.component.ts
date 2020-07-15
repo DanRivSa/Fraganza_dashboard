0b90920d704dce43c365a5877ca1ab536b656ead
DanRivSa <danielrivero2000@gmail.com>
2020-07-15 01:31:32 -0400

    login implemented

--- src/app/components/select-producer-company/select-producer-company.component.ts
+++ src/app/components/select-producer-company/select-producer-company.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserCompanyService } from 'src/app/services/global/user-company.service';
 
 @Component({
   selector: 'app-select-producer-company',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectProducerCompanyComponent implements OnInit {
 
-  constructor() { }
+  ID:number;
+
+  constructor()
+  {
+    
+  }
 
   ngOnInit(): void {
+  }
+
+  SetUser(event:any)
+  {
+    this.ID = event.target.value;
+  }
+
+  Login()
+  {
+    if(this.ID != null)
+    {
+      //asign user Company Type
+      UserCompanyService.userCompanyType = 1;
+      UserCompanyService.userCompanyID = this.ID;
+      alert('user has been identified, please return to home screen');
+    }
+    else alert('please, select a company');
   }
 
 }
--- src/app/components/select-provider-company/select-provider-company.component.ts
+++ src/app/components/select-provider-company/select-provider-company.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserCompanyService } from 'src/app/services/global/user-company.service';
 
 @Component({
   selector: 'app-select-provider-company',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectProviderCompanyComponent implements OnInit {
 
-  constructor() { }
+  ID:number;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  SetUser(event:any)
+  {
+    this.ID = event.target.value;
+  }
+
+  Login()
+  {
+    if(this.ID != null)
+    {
+      //asign user Company Type
+      UserCompanyService.userCompanyType = 2;
+      UserCompanyService.userCompanyID = this.ID;
+      alert('user has been identified, please return to home screen');
+    }
+    else alert('please, select a company');
   }
 
 }
