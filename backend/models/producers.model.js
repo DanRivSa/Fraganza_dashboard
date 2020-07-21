55ee0ef531a19acb96e7b04e6426963489f70688
DanRivSa <danielrivero2000@gmail.com>
2020-07-21 00:06:20 -0400

    se agrego el badge de uva en el catalogo de presentaciones y se aplico filtro de contrato en proveedor

--- backend/models/producers.model.js
+++ backend/models/producers.model.js
@@ -13,7 +13,7 @@ class ProducersModel
 
     async FiltrarProveedoresEvIni(id)
     {
-        const db_res = await db.query('SELECT DISTINCT p.id_prov, nombre_prov,telefono,email from ada_alternativa_envio p INNER JOIN ada_proveedor prov ON p.id_prov = prov.id_prov WHERE p.id_pais in (Select id_pais from ada_prod_pais WHERE id_prod=$1 AND p.id_prov IN (SELECT * FROM ada_prov_mem_activa))',[id]);
+        const db_res = await db.query('SELECT DISTINCT p.id_prov,nombre_prov,telefono,email from ada_alternativa_envio p INNER JOIN ada_proveedor prov ON prov.id_prov = p.id_prov WHERE p.id_pais in (Select id_pais from ada_prod_pais WHERE id_prod=$1 AND p.id_prov IN (SELECT * FROM ada_prov_mem_activa)) EXCEPT  SELECT cont.id_prov, nombre_prov,telefono,email from ada_contratos_en_regla cont INNER JOIN ada_proveedor prov ON prov.id_prov = cont.id_prov WHERE cont.id_prod = $1',[id]);
         return db_res;
     }
 }
