824a7252b541ee64cc9440300d38b58227007fe4
Andres <andresgdearmas@gmail.com>
2020-07-21 00:42:31 -0400

    Pre base para filtro de productos exclusivos

--- backend/DB_SQL/consultas.sql
+++ backend/DB_SQL/consultas.sql
@@ -35,3 +35,22 @@ SELECT nombre_prov,cas_oi,nombre_comercial,nombre_quimico FROM ada_proveedor pro
 INNER JOIN ada_otros_ing ing
 ON prov.id_prov = ing.id_prov
 WHERE prov.id_prov = $1
+
+
+
+
+--filtro para proveedores disponibles para evaluacion inicial
+SELECT DISTINCT p.id_prov,nombre_prov,telefono,email from ada_alternativa_envio p
+--seleccionar columnas
+INNER JOIN ada_proveedor prov
+ON prov.id_prov = p.id_prov
+--aplicar filtros
+WHERE p.id_pais in (Select id_pais from ada_prod_pais
+WHERE id_prod=1
+AND p.id_prov IN
+(SELECT * FROM ada_prov_mem_activa))
+EXCEPT
+SELECT cont.id_prov, nombre_prov,telefono,email from ada_contratos_en_regla cont
+INNER JOIN ada_proveedor prov
+ON prov.id_prov = cont.id_prov
+WHERE cont.id_prod = 1;
